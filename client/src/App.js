eb25982616f6b793a8144cc79e377db06d0382c6
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-01 09:42:05 -0700

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card.js';
+import DarkButton from './components/DarkButton.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* Construct the App */
+class App extends React.Component {
+  constructor() {
+    console.log("Card States Created");
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  /* Fetch Data into Initial Mounting */
+  componentDidMount() {
+    console.log("Component Successfully Mounted");
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(res => this.setState({ players: res }))
+      .catch(err => console.log(err));
+  }
+
+  /* Render Data To DOM */
+  render() {
+    console.log("Data Successfully Rendered");
+    /* Why is my console log firing twice? */
+    return (
+      <div>
+        <DarkButton />
+        <Card player={this.state.players} />
+      </div>
+    );
+  }
 }
 
 export default App;
